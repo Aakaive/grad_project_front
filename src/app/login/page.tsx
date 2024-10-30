555be2266d97dbdfb83bc46d2f6a1afffb63369e
Aakaive <wonseok031@gmail.com>
2024-10-30 16:24:09 +0900

    fix - 로그인 후 페이지 이동 안되는 현상 수정  - 로그인 후 페이지 이동 시, 짧은 딜레이 후 자동으로 새로고침 되도록 설정하여 리렌더링

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,34 +1,31 @@
-// 로그인 페이지 컴포넌트 내부
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
 import { useLogin } from '@/hooks/useLogin';
 import { useAuth } from '@/context/AuthContext';
-import { useRouter } from 'next/navigation';
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [shouldReload, setShouldReload] = useState(false); // 새로고침 상태 추가
   const { login } = useLogin();
-  const { setIsLoggedIn } = useAuth(); // 상태 변경 함수 가져오기
+  const { setIsLoggedIn } = useAuth();
   const router = useRouter();
-
-  // 로그인 후 새로고침 로직 추가
-  useEffect(() => {
-    if (shouldReload) {
-      window.location.reload(); // 새로고침
-    }
-  }, [shouldReload]);
+  const searchParams = useSearchParams();
+  const redirectUrl = searchParams.get('redirect') || '/';
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const loginSuccess = await login(email, password);
 
     if (loginSuccess) {
-      setIsLoggedIn(true); // 로그인 상태 업데이트
-      router.replace('/'); // 원래 이동하려던 페이지로 이동
-      setShouldReload(true); // 새로고침 트리거
+      setIsLoggedIn(true);
+      router.replace(redirectUrl);
+      
+      // 페이지 이동 후 새로고침
+      setTimeout(() => {
+        window.location.reload();
+      }, 100); // 짧은 딜레이로 새로고침 트리거
     } else {
       console.error('Login failed');
     }
--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,15 @@ export function middleware(request: NextRequest) {
 
   if (!hasRefreshToken && request.nextUrl.pathname.startsWith('/protected')) {
     const loginUrl = new URL('/login', request.url);
+    // 사용자가 접근하려던 URL로 돌아가도록 `redirect` 매개변수 추가
     loginUrl.searchParams.set('redirect', request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
 
-  // 로그인 상태일 경우 원하는 페이지로 계속 진행
   return NextResponse.next();
 }
 
 // 미들웨어가 적용될 경로 지정
 export const config = {
-  matcher: ['/protected/:path*'], // 필요한 보호 경로에 맞춰 수정 가능
+  matcher: ['/protected/:path*'],
 };
